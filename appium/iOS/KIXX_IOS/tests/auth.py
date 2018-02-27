1d7ddd83c3243dde750f04e5a0c2149f90b4a88c
chechetkin <32839921+BeastChe@users.noreply.github.com>
2018-02-27 15:28:45 +0300

    Вынес фикстуру отдельно в конфтест

--- appium/iOS/KIXX_IOS/tests/auth.py
+++ appium/iOS/KIXX_IOS/tests/auth.py
@@ -3,11 +3,6 @@ import pytest
 from iOS.KIXX_IOS.fixsture.application import Application
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_mail(app):
